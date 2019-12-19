e46169faff940c03f553d4bdf7f23869caa352b1
Nil Késede <nilkesede@gmail.com>
2019-12-19 15:59:25 -0300

    fix sw path and add manifest to head

--- src/components/head.js
+++ src/components/head.js
@@ -24,6 +24,7 @@ export default class Head extends Component {
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <meta name="theme-color" content="#222"/>
 
+        <link rel="manifest" href="/manifest.json"/>
         <link rel="icon" href="/favicon.ico"/>
       </NextHead>
     );
